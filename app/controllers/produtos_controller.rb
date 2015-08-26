f0472bc0f79c4004ad413e3cf644ecd13cbf289f
Maurício Medeiros <mauriciomedeiros.05@gmail.com>
2015-08-25 21:58:05 -0300

    Removido a lógica de busca dos produtos para dentro do controller

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,2 +1,7 @@
 class ProdutosController < ApplicationController
+	def index
+		## @ serve para dizer que a variável é de instancia e estarão disponívels para visualização em todo o site
+		@produtos_order_nome = Produto.order :nome
+		@produtos_order_preco = Produto.order(:preco).limit 2
+	end
 end
