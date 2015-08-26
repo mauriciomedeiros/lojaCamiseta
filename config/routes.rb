06a6cf57f6144a03b274bdfe73b55d0875612021
Maurício Medeiros <mauriciomedeiros.05@gmail.com>
2015-08-26 20:56:55 -0300

    Alterado as rotas para usar a convensão do rails com o resources, e adicionado um link para criar um novo produto

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-## get "[URI]" => "[Controller]#[nome da página]"
-  ##get "produtos" => "produtos#index"
+  ## get "[URI]" => "[Controller]#[nome da página]"
+  ## get "produtos" => "produtos#index"
   root "produtos#index"
-  get "/produtos/new" => "produtos#new"
-  post "/produtos" => "produtos#create"
-  delete "produtos/:id" => "produtos#destroy", as: :produto
+  ## get "/produtos/new" => "produtos#new"
+  ## post "/produtos" => "produtos#create"
+  ## delete "produtos/:id" => "produtos#destroy", as: :produto
+  ## todos os comentados vão ser criados pelo resources
+  resources :produtos, only: [:new, :create, :destroy]
 end
