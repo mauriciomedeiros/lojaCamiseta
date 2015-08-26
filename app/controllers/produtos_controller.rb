aa15cbcd1f38074db8f167295419c69ba5be7c2c
Maurício Medeiros <mauriciomedeiros.05@gmail.com>
2015-08-25 23:28:33 -0300

    Adicionado formulario para cadastro de produtos

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,7 +1,13 @@
 class ProdutosController < ApplicationController
 	def index
-		## @ serve para dizer que a variável é de instancia e estarão disponívels para visualização em todo o site
+		## @ serve para dizer que a variável é de instancia e estarão disponívels para visualização em todo o site.
 		@produtos_order_nome = Produto.order :nome
 		@produtos_order_preco = Produto.order(:preco).limit 2
+	end
+
+	def create
+		## O método require faz a solicitção do produto.
+		valores = params.require(:produto).permit!
+		Produto.create valores
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 ## get "[URI]" => "[Controller]#[nome da página]"
   ##get "produtos" => "produtos#index"
   root "produtos#index"
+  get "/produtos/new" => "produtos#new"
+  post "/produtos" => "produtos#create"
 end
